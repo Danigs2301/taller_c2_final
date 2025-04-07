ecc10a00413c0b1a938721c64656a26ef67dc780
Danigs2301 <danielgs1219@gmail.com>
2025-04-07 16:00:30 -0500

    ajuste errores

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/taskdb")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://atlas-sample-dataset-load-67f43bdf9a6e87041dc94102:<db_password>@cluster0.kw5obel.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
