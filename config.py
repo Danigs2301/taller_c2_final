bd034124f7b20663aad8755308e93865d01118c5
Danigs2301 <danielgs1219@gmail.com>
2025-04-21 01:48:07 -0500

    Adjust requirements

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://adm:clave123@cluster0.kw5obel.mongodb.net/taskdb?retryWrites=true&w=majority")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://adm:clave123@cluster0.kw5obel.mongodb.net/taskdb?retryWrites=true&w=majority&ssl=true")
